bda39e8c3fe598e74aa70817fbbd419d0e415106
SarikaBodkhe <88131153+SarikaBodkhe@users.noreply.github.com>
2021-08-11 01:24:44 +0530

    Create Accept one number from user and print that number of * on screen.c

---
+++ Problems on Numbers/Accept one number from user and print that number of * on screen.c
@@ -0,0 +1,25 @@
+///Accept one number from user and print that number of * on screen
+
+#include<stdio.h>
+
+ int Accept(int iNo)
+  {
+      int iCnt=0;
+
+      for(iCnt=1;iCnt<=iNo;iCnt++)
+      {
+          printf("*");
+      }
+  }
+
+int main()
+  {
+      int iValue = 0;
+
+      printf("Enter number: \n");
+      scanf("%d",&iValue);
+
+      Accept(iValue);
+
+      return 0;
+  }
