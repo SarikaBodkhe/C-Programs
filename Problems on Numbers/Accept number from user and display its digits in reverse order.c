7d370859b36ac284aba7cd99a67ea59de534fee1
SarikaBodkhe <88131153+SarikaBodkhe@users.noreply.github.com>
2021-08-11 00:39:37 +0530

    Create Accept number from user and display its digits in reverse order.c

---
+++ Problems on Numbers/Accept number from user and display its digits in reverse order.c
@@ -0,0 +1,57 @@
+/* Write a program which accept number from user and display its digits in reverse order.
+Input : 2395
+Output : 5
+ 9
+ 3
+ 2
+ 
+Input : 1018
+Output : 8
+ 1
+ 0
+ 1
+ 
+Input : -1018
+Output : 8
+ 1
+ 0
+ 1
+ 
+Input : 9000
+Output : 0
+ 0
+ 0
+ 9
+*/
+
+#include<stdio.h>
+
+void Reverse(int iNo)
+{
+    int iDigit=0;
+
+    if(iNo<0)
+    {
+        iNo=-iNo;
+    }
+
+    while(iNo!=0)
+    {
+        iDigit=iNo%10;
+        printf("%d\n",iDigit);
+        iNo=iNo/10;
+    }
+}
+
+int main()
+{
+    int iValue1=0;
+
+
+    printf("Enter no: \n");
+    scanf("%d",&iValue1);
+
+    Reverse(iValue1);
+
+    return 0;
+}
