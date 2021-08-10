9556c09974f1b5067e1a7e353cbc47b7bf1e5f2d
SarikaBodkhe <88131153+SarikaBodkhe@users.noreply.github.com>
2021-08-11 01:26:33 +0530

    Create  Accept radius of circle from user and calculate its area.c

---
+++ Problems on Numbers/Accept radius of circle from user and calculate its area.c
@@ -0,0 +1,40 @@
+/* Write a program which accept radius of circle from user and calculate its area.
+Consider value of PI as 3.14. (Area = PI * Radius * Radius)
+
+Input : 5.3
+Output : 88.2026
+
+Input : 10.4
+Output : 339.6224
+
+*/
+
+#include<stdio.h>
+
+double CircleArea(float fRadius)
+{
+    if(fRadius==0)
+    {
+        return 0;
+    }
+    
+    float fPI=3.14;
+    double dArea=0.0;
+
+    dArea=fPI*fRadius*fRadius;
+    return dArea;
+
+}
+int main()
+{
+    float fValue = 0.0;
+    double dRet=0.0;
+
+    printf("Enter radius of circle : ");
+    scanf("%f",&fValue);
+
+    dRet=CircleArea(fValue);
+    printf("Radius of circle is: %lf\n",dRet);
+
+    return 0;
+}
